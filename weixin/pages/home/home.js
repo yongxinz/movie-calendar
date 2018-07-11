602b10e66c2d7976aacee7f06752c8160c3231bf
yongxinz <hi_nature@yeah.net>
2018-07-11 14:42:07 +0800

    add ad and fix getuserinfo

--- weixin/pages/home/home.js
+++ weixin/pages/home/home.js
@@ -7,20 +7,10 @@ Page({
     },
 
     onLoad: function () {
-        let that = this;
-
-        app.helper.wxPromisify(wx.getUserInfo)().then(function (res) {
-            app.config.gData.userInfo = res.userInfo;
-            that.setData({'gData.userInfo': app.config.gData.userInfo});
-        });
+        
     },
 
     onShow: function () {
         // app.helper.waitUserSid(this.getApiData);
-    },
-
-    // 获取用户头像昵称
-    bindGetUserinfo: function (res) {
-        this.setData({'gData.userInfo': res.detail.userInfo})
     }
 });
