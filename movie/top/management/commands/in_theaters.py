f978c6af7515179e64d98ead016f40756ad2f635
yongxinz <hi_nature@yeah.net>
2018-08-06 14:15:53 +0800

    add in_theaters

---
+++ movie/top/management/commands/in_theaters.py
@@ -0,0 +1,39 @@
+# coding:utf-8
+
+import requests
+from django.core.management.base import BaseCommand
+
+from top.models import InTheaters
+
+
+class Command(BaseCommand):
+    help = 'in_theaters'
+
+    def handle(self, *args, **options):
+        InTheaters.objects.all().delete()
+
+        url = 'https://api.douban.com/v2/movie/in_theaters'
+        response = requests.get(url).json()
+        subjects = response.get('subjects', [])
+
+        for item in subjects:
+            subject = item['id']
+
+            url = 'https://api.douban.com/v2/movie/subject/' + subject
+            res = requests.get(url).json()
+
+            title = res['title']
+            countries = ','.join(res['countries'])
+            genres = ','.join(res['genres'])
+            rating = res['rating']['average']
+            stars = res['rating']['stars']
+            year = res['year']
+            directors = ','.join([m['name'] for m in res['directors']])
+            casts = ','.join([m['name'] for m in res['casts']])
+            images = res['images']['medium']
+            ratings_count = res['ratings_count']
+            summary = res['summary']
+
+            print(subject, title, countries, genres, rating, stars, year, directors, casts, images, ratings_count, summary)
+            InTheaters.objects.create(subject=subject, title=title, countries=countries, genres=genres, rating=rating, stars=stars, year=year,
+                                      directors=directors, casts=casts, images=images, ratings_count=ratings_count, summary=summary)
--- movie/top/models.py
+++ movie/top/models.py
@@ -59,3 +59,25 @@ class Tag(models.Model):
 
     class Meta:
         ordering = ['-id']
+
+
+class InTheaters(models.Model):
+    """
+    热映电影
+    """
+    subject = models.CharField(u"电影ID", max_length=15)
+    title = models.CharField(u"电影中文名", max_length=50, null=True, blank=True)
+    countries = models.CharField(u"上映城市", max_length=20, null=True, blank=True)
+    genres = models.CharField(u"电影类型", max_length=20, null=True, blank=True)
+    rating = models.CharField(u"电影评分", max_length=10, null=True, blank=True)
+    year = models.CharField(u"电影上映年份", max_length=10, null=True, blank=True)
+    directors = models.CharField(u"电影导演", max_length=50, null=True, blank=True)
+    casts = models.CharField(u"电影演员", max_length=100, null=True, blank=True)
+    images = models.CharField(u"电影海报", max_length=150, null=True, blank=True)
+    stars = models.IntegerField(u"电影评星", null=True, blank=True)
+    ratings_count = models.IntegerField(u"评价人数", null=True, blank=True)
+    summary = models.CharField(u"电影简介", max_length=1000, null=True, blank=True)
+    operate_time = models.DateTimeField(u"操作时间", auto_now=True)
+
+    class Meta:
+        ordering = ['-id']
