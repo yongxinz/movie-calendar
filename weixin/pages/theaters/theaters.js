12e079350a72121402d9a5f97e9f9ee7f527f603
yongxinz <hi_nature@yeah.net>
2018-08-06 15:08:50 +0800

    fix

--- weixin/pages/theaters/theaters.js
+++ weixin/pages/theaters/theaters.js
@@ -2,28 +2,11 @@ const app = getApp();
 
 Page({
     data: {
-        apiData: {},
-        options: {
-            page: 1
-        },
-        count: 0,
-        msg: ''
+        apiData: {}
     },
 
     onLoad: function (options) {
-        this.setData({options: options});
 
-        if (options.is_going === 'true') {
-            wx.setNavigationBarTitle({
-                title: '想看的电影'
-            });
-            this.setData({msg: '想看'});
-        } else if (options.is_done === 'true') {
-            wx.setNavigationBarTitle({
-                title: '看过的电影'
-            });
-            this.setData({msg: '看过'});
-        }
     },
 
     onShow: function () {
@@ -32,36 +15,12 @@ Page({
 
     getApiData: function () {
         let that = this;
-        that.setData({
-            'options.page': 1
-        });
 
         wx.showLoading({title: '加载中...'});
-        app.helper.getApi('theaters', that.data.options).then(function (res) {
+        app.helper.getApi('theaters').then(function (res) {
             that.setData({apiData: res.data});
-            that.setData({count: res.data.count});
         }).then(function () {
             wx.hideLoading()
         });
-    },
-
-    onReachBottom: function () {
-        let that = this;
-        if (that.data.apiData.next !== null) {
-            let next_page = that.data.options.page + 1;
-            that.setData({
-                'options.page': next_page
-            });
-
-            wx.showLoading({title: '加载中...'});
-            app.helper.getApi('retain', that.data.options).then(function (res) {
-                for (let i = 0; i < res.data.results.length; i++) {
-                    that.data.apiData.results.push(res.data.results[i])
-                }
-                that.setData({apiData: that.data.apiData, 'apiData.next': res.data.next});
-            }).then(function () {
-                wx.hideLoading()
-            });
-        }
     }
 });
